7dba2ba56a7c63654f1df5e3ef295d38e8484db9
Mohamed Hamed <mohamed@example.com>
2025-10-02 01:13:05 +0300

    Add Gi

--- tests/x.spec.js
+++ tests/x.spec.js
@@ -37,7 +37,7 @@ test.beforeEach(async ({ page }) => {
 
    // 4- اضغط على زرار Add Todo
   await page.locator('[data-testid="add"]').click();
- const todoText = "dzddbdzgxxxgsexxxxdwd5aaesxzar";
+ const todoText = "dzddbdzgxxxgsexrrrrxxxdwd5aaesxzar";
   // 5- اكتب التودو الجديد
   await page.locator('[data-testid="new-todo"]').fill(todoText);
   await page.locator('[data-testid="submit-newTask"]').click();
